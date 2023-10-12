6880037675ca98bdc9c8c61f47c80969a171762f
shiv-pencia <146872972+shiv-pencia@users.noreply.github.com>
2023-10-12 05:58:34 +0000

    add ForLoop

--- react.js
+++ react.js
@@ -2,8 +2,13 @@
 function customRender(reactElement,container){
     const domElement = document.createElement(reactElement.type)
     domElement.innerHTML = reactElement.children
-    domElement.setAttribute('href',reactElement.props.href)
-    domElement.setAttribute('target',reactElement.props.target)
+    for (const props in reactElement.props) {
+        if (props==='children') continue;
+
+            domElement.setAttribute(props , reactElement.props[props])
+            
+        
+    }
     
     container.appendChild(domElement)
 }
